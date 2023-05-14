edd9883e03e5b7b4a3eac001ed614e2f48923f75
Tyler Rhodes <rhodetyl000@gmail.com>
2023-05-14 16:36:39 -0700

    Define import functions from module explicitly

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -38,7 +38,7 @@ defmodule Servy.Handler do
   @moduledoc "Handles HTTP requests."
 
   @pages_path Path.expand("../../pages", __DIR__)
-  import Servy.Plugins
+  import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
 
   @doc "Transforms the request into a response."
   def handle(request) do
