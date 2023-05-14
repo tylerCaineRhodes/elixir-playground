0f25e91da6fc948a8d8a060ce55059dd9fc03bd6
Tyler Rhodes <rhodetyl000@gmail.com>
2023-05-14 00:00:11 -0700

    Prefer function clauses over case statements

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -70,24 +70,26 @@ defmodule Servy.Handler do
   end
 
   def route(%{method: "GET", path: "/about"} = conv) do
-    file =
-      Path.expand("../../pages", __DIR__)
-      |> Path.join("about.html")
-
-    case File.read(file) do
-      {:ok, content} ->
-        %{conv | status: 200, resp_body: content}
-
-      {:error, :enoent} ->
-        %{conv | status: 404, resp_body: "File not found!"}
-
-      {:error, reason} ->
-        %{conv | status: 500, resp_body: "File error #{reason}"}
-    end
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
   end
 
   def route(%{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here"}
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{conv | status: 200, resp_body: content}
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{conv | status: 404, resp_body: "File not found!"}
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{conv | status: 500, resp_body: "File error #{reason}"}
   end
 
   def format_response(conv) do
