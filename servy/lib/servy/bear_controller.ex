603794b2677435dfc3cd768aa7cf09d71e8f9381
Tyler Rhodes <rhodetyl000@gmail.com>
2023-06-13 08:52:06 -0700

    Utilize shorthand syntax with arrity

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -9,9 +9,9 @@ defmodule Servy.BearController do
   def index(conv) do
     items =
       Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly?(&1))
-      |> Enum.sort(&Bear.order_asc_by_name(&1, &2))
-      |> Enum.map(&bear_item(&1))
+      |> Enum.filter(&Bear.is_grizzly?/1)
+      |> Enum.sort(&Bear.order_asc_by_name/2)
+      |> Enum.map(&bear_item/1)
       |> Enum.join()
 
     %{conv | status: 200, resp_body: "<ul><li>#{items}</li></ul>"}
