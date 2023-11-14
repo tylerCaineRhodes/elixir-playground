4920dc1247082b42e418b000923f7b824e9bbb1e
Tyler Rhodes <rhodetyl000@gmail.com>
2023-11-13 22:36:54 -0700

    Create generic render

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -1,18 +1,22 @@
 defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
-  alias Servy.BearView
+  # alias Servy.BearView
+
+  import Servy.View, only: [render: 3]
 
   def index(conv) do
-    bears = Wildthings.list_bears() |> Enum.sort(&Bear.order_asc_by_name/2)
+    bears =
+      Wildthings.list_bears()
+      |> Enum.sort(&Bear.order_asc_by_name/2)
 
-    %{conv | status: 200, resp_body: BearView.index(bears)}
+    render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
 
-    %{conv | status: 200, resp_body: BearView.show(bear)}
+    render(conv, "show.eex", bear: bear)
   end
 
   def create(conv, %{"name" => name, "type" => type}) do
