c4ea25ed83e9cefcca6d0d75aa1824e2b00e7b33
Tyler Rhodes <rhodetyl000@gmail.com>
2023-11-13 21:50:26 -0700

    Create view-generated functions

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -1,24 +1,18 @@
 defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
-
-  @templates_path Path.expand("../../tempates", __DIR__)
-
-  defp render(conv, template, bindings \\ []) do
-    content = @templates_path |> Path.join("index.eex") |> EEx.eval_file(bindings)
-    %{conv | status: 200, resp_body: content}
-  end
+  alias Servy.BearView
 
   def index(conv) do
     bears = Wildthings.list_bears() |> Enum.sort(&Bear.order_asc_by_name/2)
 
-    render(conv, "index.eex", bears: bears)
+    %{conv | status: 200, resp_body: BearView.index(bears)}
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
 
-    render(conv, "show.eex", bear: bear)
+    %{conv | status: 200, resp_body: BearView.show(bear)}
   end
 
   def create(conv, %{"name" => name, "type" => type}) do
