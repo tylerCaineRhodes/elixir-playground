425e64a8416ab319959e9b30f5e52f8c4fe1f319
Tyler Rhodes <rhodetyl000@gmail.com>
2023-11-13 21:32:56 -0700

    Use eex views

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -2,24 +2,23 @@ defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
 
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
+  @templates_path Path.expand("../../tempates", __DIR__)
+
+  defp render(conv, template, bindings \\ []) do
+    content = @templates_path |> Path.join("index.eex") |> EEx.eval_file(bindings)
+    %{conv | status: 200, resp_body: content}
   end
 
   def index(conv) do
-    items =
-      Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly?/1)
-      |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
-      |> Enum.join()
+    bears = Wildthings.list_bears() |> Enum.sort(&Bear.order_asc_by_name/2)
 
-    %{conv | status: 200, resp_body: "<ul><li>#{items}</li></ul>"}
+    render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    %{conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}</h1>"}
+
+    render(conv, "show.eex", bear: bear)
   end
 
   def create(conv, %{"name" => name, "type" => type}) do
@@ -30,6 +29,11 @@ defmodule Servy.BearController do
     }
   end
 
+  @spec delete(%{:resp_body => any(), :status => any(), optional(any()) => any()}, any()) :: %{
+          :resp_body => <<_::232>>,
+          :status => 403,
+          optional(any()) => any()
+        }
   def delete(conv, _params) do
     %{conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
   end
